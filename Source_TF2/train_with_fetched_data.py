65643d2e1efec590972460acc5a3f054f78dee0a
User <user@example.com>
2025-11-28 18:45:42 -0500

    Fixed warning

--- Source_TF2/train_with_fetched_data.py
+++ Source_TF2/train_with_fetched_data.py
@@ -17,7 +17,7 @@ NumAction = 3
 W = 32  # Image size
 
 # Training hyperparameters
-maxiter = 500000        # iterations (paper uses 5M)
+maxiter = 100000        # iterations (paper uses 5M)
 learning_rate = 0.00001
 epsilon_min = 0.1
 M = 1000                # memory buffer capacity
